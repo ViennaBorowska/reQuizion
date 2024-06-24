604d064f08a08b5cd9219187fdaef8353d5f20cc
Vienna B <viennabdev@gmail.com>
2024-06-24 16:08:24 +0100

    setup: homepage buttons added

--- requizion/screens/Homepage.js
+++ requizion/screens/Homepage.js
@@ -1,26 +1,41 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View, Image } from "react-native";
+import { Button, Text, StyleSheet, View, Image } from "react-native";
 import header from "../images/REQUIZION.jpg";
 
 const Homepage = () => {
   return (
-    <View>
+    <View style={{ alignContent: "center" }}>
       <Image
         style={{ height: 300, width: "100%", resizeMode: "contain" }}
         source={require("../images/REQUIZION.jpg")}
       />
       <View style={{ padding: 10 }}>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>
-          Welcome to the Quiz App
+        <Text style={styles.text.heading}>Welcome to Requizion</Text>
+        <Text style={styles.text.body}>
+          Browse available quizzes by subject below, or visit 'My Quizzes'
+          create & play your own custom quiz!
         </Text>
-        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-          This is a Quiz App
-        </Text>
+      </View>
+      <View style={{ padding: 10 }}>
+        <Button title={"Browse Quizzes"} onPress={() => handlePress(Browse)} />
+        <Button title={"Your Quizzes"} onPress={() => handlePress(UserQuiz)} />
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    heading: {
+      fontSize: 30,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    body: {
+      fontSize: 15,
+      textAlign: "center",
+    },
+  },
+});
 
 export default Homepage;
