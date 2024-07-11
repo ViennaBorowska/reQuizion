2644c4b7dc2ab11404747d2f48a507caa9f6e1a5
Vienna B <viennabdev@gmail.com>
2024-07-11 15:27:55 +0200

    basic data added for 4 quizzes

---
+++ requizion/data/englishlang.js
@@ -0,0 +1,139 @@
+export default [
+  {
+    question:
+      "Despite [...INSERT ANSWER...] chosen for the team, Raoul preferred playing computer games to spending time training.",
+    options: [
+      {
+        id: "0",
+        options: "A",
+        answer: "Having been",
+      },
+      {
+        id: "1",
+        options: "B",
+        answer: "Been",
+      },
+      {
+        id: "2",
+        options: "C",
+        answer: "He was",
+      },
+      {
+        id: "3",
+        options: "D",
+        answer: "Was",
+      },
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    question: "Which of the following is NOT correct?",
+    options: [
+      {
+        id: "0",
+        options: "A",
+        answer:
+          "It's Mum's birthday tomorrow, so Dad asked us to make her a cake.",
+      },
+      {
+        id: "1",
+        options: "B",
+        answer:
+          "It's Mum's birthday tomorrow, so Dad asked me and Jenna to make her a cake.",
+      },
+      {
+        id: "2",
+        options: "C",
+        answer:
+          "It's Mum's birthday tomorrow, so Jenna and I are going to make her a cake.",
+      },
+      {
+        id: "3",
+        options: "D",
+        answer:
+          "It's Mum's birthday tomorrow, so Dad asked Jenna and I to make her a cake.",
+      },
+    ],
+    correctAnswerIndex: 3,
+  },
+  {
+    question:
+      "[...INSERT ANSWER...] the mess she had just made of her bedroom, Stephanie's offer to tidy the kitchen seemed somewhat suspect.",
+    options: [
+      {
+        id: "0",
+        options: "A",
+        answer: "Despite",
+      },
+      {
+        id: "1",
+        options: "B",
+        answer: "Consequently",
+      },
+      {
+        id: "2",
+        options: "C",
+        answer: "Given",
+      },
+      {
+        id: "3",
+        options: "D",
+        answer: "Because",
+      },
+    ],
+    correctAnswerIndex: 2,
+  },
+  {
+    question:
+      "[...INSERT ANSWER...] too quiet outside: [...INSERT ANSWER...]  almost as if the road and all [...INSERT ANSWER...]  traffic have disappeared.",
+    options: [
+      {
+        id: "0",
+        options: "A",
+        answer: "Its, its, it's",
+      },
+      {
+        id: "1",
+        options: "B",
+        answer: "It's, its, its",
+      },
+      {
+        id: "2",
+        options: "C",
+        answer: "It's, it's, it's",
+      },
+      {
+        id: "3",
+        options: "D",
+        answer: "It's, it's, its",
+      },
+    ],
+    correctAnswerIndex: 3,
+  },
+  {
+    question: "Which of the following is NOT correct?",
+    options: [
+      {
+        id: "0",
+        options: "A",
+        answer: "Bill and I took a walk by the river this morning.",
+      },
+      {
+        id: "1",
+        options: "B",
+        answer: "We took a walk by the river this morning.",
+      },
+      {
+        id: "2",
+        options: "C",
+        answer: "Me and Bill took a walk by the river this morning.",
+      },
+      {
+        id: "3",
+        options: "D",
+        answer: "He and I took a walk by the river this morning.",
+      },
+    ],
+    correctAnswerIndex: 2,
+  },
+];
